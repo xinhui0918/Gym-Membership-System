0b43d495a9e7f39ca53d3dd7f3bd510441d92b5c
R3su1t <tjunyu8@gmail.com>
2026-08-20 17:15:56 +0800

    Update UserMenu.cpp

--- Gym Membership System/UserMenu.cpp
+++ Gym Membership System/UserMenu.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
+#include <cctype>
 using namespace std;
 
 //User Information
 struct GymUser
 {
-	int userID;
-	string userName;
-	string phoneNum;
-	int packageID;
+	string userID = "";
+	string userName = "";
+	string phoneNum = "";
+	string packageID = "";
 };
 
-//array maximum number user
-const int max_user = 50;
-
-//UserData
-GymUser users[max_user] = {
-
-	{ 0001, "Tan", "0193257193", 1001},
-	{ 0002, "Loh", "0138642947", 1002},
-	{ 0003, "Ooi", "0163841946", 1004},
-
+//Package Information
+struct GymPackage
+{
+	string packageID = "";
 };
-int user_count = 3;
+
+//User
+const int max_user = 50;//array maximum number user
+GymUser users[max_user];
+int user_count = 0;
+
+//Package 
+const int max_package = 50;//array maximum number user
+GymPackage packages[max_package];
+int package_count = 0;
 
 // function prototypes
 void displayHeader(string title);
@@ -31,14 +36,61 @@ void userMenu();
 void clearInputBuffer();
 int getMenuChoice(int low, int hight);
 
-// function Member
-void user();
+// function User Membership
 void addUser();
 void updateUser();
 void deleteUser();
 void searchUser();
 void displayUser();
+void promptUser();
 
+//function integer number
+void isNumber();
+
+//data.txt file function
+void LoadUserFromFile();
+void SaveUserToFile();
+
+void LoadUserFromFile() {
+	ifstream inData("UserData.txt");
+	
+	if (!inData) {
+		users[0] = { "0001", "Tan", "0193257193","1001"},
+		users[1] = { "0002", "Loh", "0138642947","1001"},
+		users[2] = { "0003", "Ooi", "0163841946","1004"},
+
+		user_count = 3;
+		SaveUserToFile();
+		return;
+	}
+
+	while (user_count < max_user && inData >> users[user_count].userID) {
+		inData >> users[user_count].userName;
+		inData >> users[user_count].phoneNum;
+		inData >> users[user_count].packageID;
+
+		user_count++;
+	}
+
+	inData.close();
+};
+
+void SaveUserToFile() {
+	ofstream outData("UserData.txt");
+
+	if (!outData) {
+		cout << "[Error] Unable to open data file" << endl;
+		return;
+	}
+	for (int i = 0; i < max_user; i++) {
+		outData << users[i].userID << endl;
+		outData << users[i].userName << endl;
+		outData << users[i].phoneNum << endl;
+		outData << users[i].packageID << endl;
+	}
+
+	outData.close();
+}
 
 //Display User Menu Header
 void displayHeader(string title)
@@ -49,7 +101,7 @@ void displayHeader(string title)
 }
 
 // Display User menu
-void userMenu() {
+void promptUser() {
 	cout << "| 1. Add User                            |" << endl;
 	cout << "| 2. Update User                         |" << endl;
 	cout << "| 3. Delete User                         |" << endl;
@@ -89,9 +141,11 @@ int getMenuChoice(int low, int high)
     return choice;
 }
 
+
+
 //user valid ID exist
-bool IsValidUserID(int userID) {
-	for (int i = 0; i <= user_count; i++) {
+bool IsValidUserID(string userID) {
+	for (int i = 0; i < user_count; i++) {
 		if (users[i].userID == userID) {
 			return true;
 		};
@@ -110,7 +164,7 @@ bool IsValidPhoneNum(string phoneNum) {
 }
 
 //package valid packageID exist
-bool IsValidPackageID(int packageID) {
+bool IsValidPackageID(string packageID) {
 	for (int i = 0; i < user_count; i++) {
 		if (users[i].packageID == packageID) {
 			return true;
@@ -118,15 +172,32 @@ bool IsValidPackageID(int packageID) {
 	}
 	return false;
 }
+
+//just approve the number
+bool isNumber(string userID) {
+	if (userID.empty()) {
+		return false;
+	}
+	for (int i = 0; i < userID.length(); i++) {
+		if (!isdigit(userID[i])) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+}
 //valid first number = 0
 
-void user() {
+void userMenu() {
 	int userChoice;
 	do{
 		cout << "\n==========================================\n";
 		cout << "|                 User Menu              |";
 		cout << "\n==========================================\n";
-		userMenu();
+
+		promptUser();
 		userChoice = getMenuChoice(1, 6);
 		switch (userChoice)
 		{
@@ -147,7 +218,7 @@ void user() {
 			displayUser();
 			break;
 		case 6:
-			userMenu();
+			cout << "Exiting User Menu" << endl ;
 			break;
 		default:
 			cout << "Please enter 1-6" << endl;
@@ -162,25 +233,29 @@ void user() {
 void addUser() {
 
 	displayHeader("Add UserID");
-	int userID;
-	string userName;
-	string phoneNum;
-	int packageID;
 
 	// > max_user
 	if ( user_count >= max_user) {
 		cout << "Our User member are full" << endl;
-		return user();
+		return;
 	}
 
 	// Add userID
-	GymUser users;
+	GymUser newUser;
+	cout << "Enter UserID(first Num = 0): ";
 	while(true) {
-		cout << "Enter UserID(first Num = 0): ";
-		cin >> users.userID;
-		if (IsValidUserID(users.userID)) {
-			cout << "Please Enter Full Number New UserID" << endl;
+		cin >> newUser.userID;
+
+		if (IsValidUserID(newUser.userID)) {
+			cout << "UserID already exist. Please Try Again:" << endl;
 			clearInputBuffer();
+		}
+		else if (newUser.userID[0] != '0') {
+			cout << "First Number start with (0):";
+
+		}
+		else if (!isNumber(newUser.userID)) {
+			cout << "UserID must contain numbers only!" << endl;
 		}
 		else {
 			break;
@@ -189,28 +264,25 @@ void addUser() {
 	}
 	 
 	//Add userName
-	cout << "Enter Username (MAX 20 character): " << endl;
-	cin >> users.userName;
-	while (users.userName.length() > 20) {
-		cout << "Username must below 20 charaters" << endl;
+	cout << "Enter Username (MAX 4 character): " << endl;
+	cin >> newUser.userName;
+	while (newUser.userName.length() > 4 || newUser.userName.empty()) {
+		cout << "Username must below 5 charaters" << endl;
 		cout << "Enter Username Again: ";
-		cin >> users.userName;
+		cin >> newUser.userName;
 	};
 
 	//Add PhoneNumber
 	cout << "Enter PhoneNumber(MAX 11 character): " << endl;
 
 	while(true) {
-		cin >> users.phoneNum;
-		while (users.phoneNum.length() > 11 || users.phoneNum.length() < 10) {
+		cin >> newUser.phoneNum;
+		if (newUser.phoneNum.length() > 11 || newUser.phoneNum.length() < 10) {
 			cout << "PhoneNumber must 10 - 11 character" << endl;
-			cout << "Enter PhoneNumber Again: ";
-			cin >> users.phoneNum;
+			cout << "Please try Again: ";
 		}
-	
-		if (IsValidPhoneNum(users.phoneNum)) {
-			cout << "Phone Number had been used" << endl;
-			cout << "Please Enter Again: ";
+		else if (IsValidPhoneNum(newUser.phoneNum)) {
+			cout << "Phone Number had been used. Please Try Again:" << endl;
 		}
 		else {
 			break;
@@ -219,15 +291,20 @@ void addUser() {
 
 	//Add packageID
 	cout << "Enter PackageID: " << endl;
-	cin >> users.packageID;
-	if (IsValidPackageID(users.packageID)) {
+	cin >> newUser.packageID;
+	if (IsValidPackageID(newUser.packageID)) {
+
+		users[user_count] = newUser;
+		user_count++;
+		SaveUserToFile();
+
 		cout << "\n========================================\n";
 		cout << "|        USER ADDED SUCCESSFULLY!      |\n";
 		cout << "========================================\n";
-		cout << "| User ID       : " << users.userID << "                    |" << endl;
-		cout << "| Username      : " << users.userName << "                 |" << endl;
-		cout << "| Phone Number  : " << users.phoneNum << "                 |" << endl;
-		cout << "| Package ID    : " << users.packageID << "                 |" << endl;
+		cout << " User ID       : " << newUser.userID << endl;
+		cout << " Username      : " << newUser.userName << endl;
+		cout << " Phone Number  : " << newUser.phoneNum << endl;
+		cout << " Package ID    : " << newUser.packageID << endl;
 		cout << "========================================\n";
 	}
 	else {
@@ -241,13 +318,13 @@ void addUser() {
 
 void updateUser() {
 	displayHeader("Update User");
-	int userid;
-	int packageid;
+	string userid;
+	string packageid;
 	string phonenum;
 	
 	cout << "Enter your ID: ";
 	cin >> userid;
-	for (int i = 0; i <= user_count; i++) {
+	for (int i = 0; i < user_count; i++) {
 		if (users[i].userID == userid) {
 			cout << "\n Found The UserID! \n";
 
@@ -263,13 +340,15 @@ void updateUser() {
 					cout << "Please Enter New Phone Number : ";
 				}
 				else {
+					users[i].phoneNum = phonenum;
 					break;
 				}
 			}
 
-			cout << "Enter NewPackageID: ";
+			cout << "Enter current packageID: ";
 			cin >> packageid;
 			if (users[i].packageID == packageid) {
+				SaveUserToFile();
 				cout << "\n========================================\n";
 				cout << "|        USER UPDATE SUCCESSFULLY!      |\n";
 				cout << "========================================\n";
@@ -282,6 +361,11 @@ void updateUser() {
 			else {
 				cout << "Invalid PacakageID";
 			}
+			return;
+		}
+		else {
+			cout << "User ID not found!" << endl;
+			break;
 		}
 	}
 }
@@ -289,21 +373,26 @@ void updateUser() {
 void deleteUser() {
 	displayHeader("Delete User");
 
-	int userid ;
+	string userid ;
 	cout << "Enter Existing UserID ";
 	cin >> userid;
 
-	for (int i = 0; i <= user_count; i++) {
+	for (int i = 0; i < user_count; i++) {
 		if (users[i].userID == userid) {
 
 			for (int j = i; j < user_count - 1; j++) {
-				users[i] = users[j + 1];
+				users[j] = users[j + 1];
 			}
 		user_count--;
+		SaveUserToFile();
 		
 		cout << "DELETE SUCCESSFULLY!" << endl;
-		break;
+		return;
 		
+		}
+		else {
+			cout << "User ID not found!" << endl;
+			break;
 		}
 	}
 	
@@ -313,25 +402,28 @@ void deleteUser() {
 void searchUser() {
 	displayHeader("Search User");
 
-	int userid;
+	string userid;
 	cout << "Enter UserID: ";
 	cin >> userid;
 	for (int i = 0; i < user_count; i++) {
-		if (users[i].userID = userid) {
+		if (users[i].userID == userid) {
 			cout << "UserID: " << users[i].userID << endl;
 			cout << "Username: " << users[i].userName << endl;
 			cout << "Phone Number: " << users[i].phoneNum << endl;
 			cout << "Package ID: " << users[i].packageID << endl;
 			return;
 		}
-
+		else {
+			cout << "User ID not found!" << endl;
+			break;
+		}
 	}
 }
 
 void displayUser() {
 	displayHeader("Display All User");
 
-	for (int i = 0; i <= user_count; i++) {
+	for (int i = 0; i < user_count; i++) {
 		cout << left
 			<< setw(5) << users[i].userID 
 			<< setw(7) << users[i].userName
@@ -342,6 +434,7 @@ void displayUser() {
 }
 
 int main() {
-	user();
+	LoadUserFromFile();
+	userMenu();
 	return 0;
 }
